251e254fc95a3845995f1a025f95124c4f4b7047
abidhossain15 <abidhossain3000@gmail.com>
2021-07-12 13:47:35 +0600

    google login button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const handleGoogle=()=>{
+    const url = `https://apimaterialbuy.herokuapp.com/users/google`
+    // axios.get(url)
+    // .then(res=>console.log(res.data))
+    window.open(url, "_blank", 'width=500px,height=500px');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleGoogle} >Google sign in</button>
     </div>
   );
 }
